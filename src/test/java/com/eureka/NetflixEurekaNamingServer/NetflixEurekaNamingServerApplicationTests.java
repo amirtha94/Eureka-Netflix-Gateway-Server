7f0fd53ea0625d6599f543e31747fda0c231c9e0
Amirtha S <amirtha1994@gmail.com>
2020-04-28 17:06:44 +0530

    Eureka gateway

---
+++ src/test/java/com/eureka/NetflixEurekaNamingServer/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.NetflixEurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
